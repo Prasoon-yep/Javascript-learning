b8e2aef02b37d67284c6ea57bf3950ed0167d214
Prasoon-yep <wwwprasoonkcw@gmail.com>
2024-10-04 03:41:30 +0000

    varandconst

---
+++ 01_basics/variableandconst.js
@@ -0,0 +1,9 @@
+const accountID = 12334
+let accountEmail ="prasoonkcw@gmail.com"
+var accountPassword ="12345"
+accountCity= "Daldale"
+
+// accountID = 2 //const can't be change
+
+console.log(accountID);
+console .table([accountEmail,accountPassword,accountCity])
